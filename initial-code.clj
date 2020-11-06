5d5118a8ce6d4a37446d5e4436905eca2cc56e57
Shortrope <mario@shortrope.com>
2020-11-06 07:33:17 -0800

    Define some data to easily copy paste into the repl
        initial-code.clj

---
+++ initial-code.clj
@@ -0,0 +1,8 @@
+(def mylist '("list zero item" "list item one" "list item two"))
+(def myvec ["vec zero item" "vec item one" "vec item two"])
+(def myset #{1 2 3 5 8 11})
+(def mymap {:a "aay", :b "bee", :f "eph", :j "jay"}) 
+(def nestedmap {:locations {
+                    :san_diego {:lat 45.1234, :lon 35.4321}
+                    :los_angeles {:lat 44.1234, :lon 34.4321}
+                    :las_vegas {:lat 43.1234, :lon 33.4321} }})
