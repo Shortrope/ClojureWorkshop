7ce10edcfebbefc90ce771b7b6606ec30c90f143
Shortrope <mario@shortrope.com>
2020-11-07 07:31:39 -0800

    parenthmazes2.clj: add strike fn to use the weapon-fn-map

--- Chap3/parenthmazes2.clj
+++ Chap3/parenthmazes2.clj
@@ -14,4 +14,38 @@
         :staff (partial + 35)
         :sword #(- % 100)
         :saucepan #(- % 100 (rand-int 50))
+        :potatoe identity
     })
+
+(defn strike 
+    ([target] (strike target :fists))
+    ([target weapon]
+        ;(let [newhealth ((weapon-fn-map weapon) (:health target))]
+            ;(assoc target :heath newhealth))))
+        (let [weapon-fn (weapon weapon-fn-map)]
+            (update target :health weapon-fn))))
+
+(def ally {
+    :name "Carla" 
+    :health 80 
+    :camp :gnomes
+})
+(def enemy {
+    :name "Zulkaz" 
+    :health 250 
+    :armor 0.8 
+    :camp :trolls
+})
+
+(strike ally)
+(strike ally :fists)
+(strike ally :staff)
+(strike ally :sword)
+(strike ally :saucepan)
+(strike ally :potatoe)
+(strike enemy)
+(strike enemy :fists)
+(strike enemy :staff)
+(strike enemy :sword)
+(strike enemy :saucepan)
+(strike enemy :potatoe)
